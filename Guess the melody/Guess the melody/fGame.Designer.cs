4e8678ef74a12846d080faa2f724383754237e7b
testuserr <troykak@mail.ru>
2017-02-01 10:33:03 +0200

    create fGame form

---
+++ Guess the melody/Guess the melody/fGame.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Guess_the_melody
+{
+    partial class fGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Guess_the_melody.Properties.Resources._3D_Music_Words_Wallpaper__E_Entertainment;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "fGame";
+            this.Text = "fGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Guess the melody/Guess the melody/fGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_the_melody
+{
+    public partial class fGame : Form
+    {
+        public fGame()
+        {
+            InitializeComponent();
+        }
+    }
+}

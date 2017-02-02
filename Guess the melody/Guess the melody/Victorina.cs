a6abcadb0373b0b79fbb32127db80862dc23d4d9
testuserr <troykak@mail.ru>
2017-02-02 11:23:46 +0200

    add additional parameters; add ReadMusic func for init list of melodies

--- Guess the melody/Guess the melody/Victorina.cs
+++ Guess the melody/Guess the melody/Victorina.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Guess_the_melody
 {
     static class Victorina
     {
         static public List<string> list = new List<string>();
+        static public int gameDuration;
+        static public int musicDuration;
+        static public bool randonStart = false;
+        static public string lastFolder = "";
+        static public bool AllDirectories = false;
+
+        static public void ReadMusic()
+        {
+            string[] music_list = System.IO.Directory.GetFiles(lastFolder, "*.mp3",
+                    AllDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            list.Clear();
+            list.AddRange(music_list);
+        }
     }
 }

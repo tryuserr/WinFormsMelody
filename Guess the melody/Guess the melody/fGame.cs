2d83a34c499367428bfc800a73cccb6b3328205b
testuserr <troykak@mail.ru>
2017-02-02 14:58:19 +0200

    add handlers, for controls

--- Guess the melody/Guess the melody/fGame.cs
+++ Guess the melody/Guess the melody/fGame.cs
@@ -12,14 +12,61 @@ namespace Guess_the_melody
 {
     public partial class fGame : Form
     {
+        Random rnd;
         public fGame()
         {
             InitializeComponent();
+            rnd = new Random();
+        }
+
+        void MakeMusic()
+        {
+            int n = rnd.Next(0, Victorina.list.Count - 1);
+            WMP.URL = Victorina.list[n];
+            Victorina.list.RemoveAt(n);
+
+            label_melodycounter.Text = Victorina.list.Count.ToString();
         }
 
         private void button_nextmelody_Click(object sender, EventArgs e)
         {
+            MakeMusic();
+            timer.Start();
+        }
 
+        private void button_pause_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            WMP.Ctlcontrols.pause();
+        }
+
+        private void button_continue_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            WMP.Ctlcontrols.play();
+        }
+
+        private void fGame_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer.Stop();
+            WMP.Ctlcontrols.stop();
+        }
+
+        private void fGame_Load(object sender, EventArgs e)
+        {
+            label_melodycounter.Text = Victorina.list.Count.ToString();
+            progressBar.Value = 0;
+            progressBar.Maximum = Victorina.gameDuration;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value++;
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                timer.Stop();
+                WMP.Ctlcontrols.stop();
+            }
         }
     }
 }

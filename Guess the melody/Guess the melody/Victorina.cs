96cbcb9575f49616e118343779273edb13428463
testuserr <troykak@mail.ru>
2017-02-02 11:06:54 +0200

    create Victorina class

---
+++ Guess the melody/Guess the melody/Victorina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_the_melody
+{
+    static class Victorina
+    {
+        static public List<string> list = new List<string>();
+    }
+}
